050ea2119e847a730dbe718ef9ff42d34fc779c7
zhuzl3 <zhuzl3@asiainfo.com>
2021-07-20 10:19:03 +0800

    chore(README.md): 更新说明
    style(/main.js):删除不必要的注释

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@ import App from './App.vue'
 import store from './store'
 import ViewUI from 'view-design';
 import './styles/icon.css';
-// import router from './router'
-
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
-// use
 import 'view-design/dist/styles/iview.css';
 import router from './router'
 import axios from 'axios';
@@ -46,11 +43,7 @@ new Vue({
   router,
   store,
   mounted(){
-    // if(localStorage)
-    // 这里需要做判断是否是owner角色登录
     this.$store.commit('initMenuList',  appRouters);
-    // ! test only
-    // this.$store.commit('updateMenuList');
   },
   render: h => h(App)
 }).$mount('#app')
