b2aabe3974f6b044d9f9ad25bb60b2785f5e4f06
zhuzl3 <zhuzl3@asiainfo.com>
2021-08-03 15:40:31 +0800

    chore：将util限制上传

--- src/util/util.js
+++ src/util/util.js
@@ -9,8 +9,8 @@ export let client = new OSS({
   // region以杭州为例（oss-cn-hangzhou），其他region按实际情况填写。
   region: "oss-cn-beijing",
   // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-  accessKeyId: "<YourID>",
-  accessKeySecret: "<YourSecret>",
+  accessKeyId: "LTAI5tA33Cm4YeF6sKAqWCTt",
+  accessKeySecret: "V6xMtBVHwnPKlRS9xzjSazisPA42zY",
   bucket: "bemountimgbed",
 });
 
@@ -24,7 +24,7 @@ export const baseUserNameChangeMenuList = (vm) =>{
 }
 
 export const turnUTCToGMT = utcTime =>{
-    // let date = new Date(utcTime);
+
     return moment(utcTime).format('YYYY-MM-DD HH:mm');
 }
 
